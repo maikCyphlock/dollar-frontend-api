eb2136c5b3215073e115d40816517c3f81863d8a
maikCyphlock <maikolaguilar656@gmail.com>
2023-01-07 20:52:23 -0400

    first version of frontend api Dollar ve

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+import preact from '@astrojs/preact'
+
+// https://astro.build/config
+import compress from 'astro-compress'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), compress()]
+})
---
+++ src/components/ThemeChanger.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "preact/hooks";
+import type { FunctionalComponent } from "preact";
+
+
+export default function ThemeToggle(): any {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <button class="theme-changer-btn" onClick={handleClick} >
+
+      {
+        theme === "light"
+          ? <SunIcon />
+          : <MoonIcon />
+
+      }
+    </button>
+  );
+}
+
+
+
+
+
+const SunIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: inherit;transform: ;msFilter:;"><path d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"></path></svg>
+)
+
+const MoonIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: inherit;transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg>
+)
