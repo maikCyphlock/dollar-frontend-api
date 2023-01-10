a9091622c5f3073ee0b45dc83a08267d917a41ff
maikCyphlock <maikolaguilar656@gmail.com>
2023-01-09 21:21:58 -0400

    removing unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import preact from '@astrojs/preact'
-
-// https://astro.build/config
 import compress from 'astro-compress'
 
 // https://astro.build/config
+import react from '@astrojs/react'
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), compress()]
+  integrations: [compress(), react()]
 })
